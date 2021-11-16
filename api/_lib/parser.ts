93d7ab437e73eb6f2e52e83bce40f59ae22d3c3f
Ismail Habibi Herman <ismail.habibi2104@gmail.com>
2021-11-16 23:32:17 +0700

    fix: error build

--- api/_lib/parser.ts
+++ api/_lib/parser.ts
@@ -1,6 +1,6 @@
 import { IncomingMessage } from 'http';
 import { parse } from 'url';
-import { ParsedRequest, Theme } from './types';
+import { ParsedRequest } from './types';
 
 export function parseRequest(req: IncomingMessage) {
     console.log('HTTP ' + req.url);
@@ -36,7 +36,7 @@ export function parseRequest(req: IncomingMessage) {
         widths: getArray(widths),
         heights: getArray(heights),
     };
-    parsedRequest.images = getDefaultImages(parsedRequest.images, parsedRequest.theme);
+    parsedRequest.images = getDefaultImages(parsedRequest.images);
     return parsedRequest;
 }
 
@@ -50,7 +50,7 @@ function getArray(stringOrArray: string[] | string | undefined): string[] {
     }
 }
 
-function getDefaultImages(images: string[], theme?: Theme): string[] {
+function getDefaultImages(images: string[]): string[] {
     const defaultImage = 'https://ismlhbb.xyz/logo.png'
 
     if (!images || !images[0]) {
